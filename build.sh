72249fab315a03f3830412690871444c1a213fdd
Stefan Gasterstädt <ghostcity@users.noreply.github.com>
2019-10-12 01:38:23 +0200

    Increment project version like this

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 # mvn clean verify      --projects parent-pom --also-make-dependents
   mvn clean verify site --projects parent-pom --also-make-dependents
 # mvn clean verify site --projects parent-pom --also-make-dependents | tee build.log
+
+# mvn versions:set -DnewVersion=0.0.2-SNAPSHOT -DgenerateBackupPoms=false --projects parent-pom --also-make-dependents
